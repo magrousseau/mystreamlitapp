a78614848e9ece89faef2d894e648a93ec41c2b0
Magali <70401848+magrousseau@users.noreply.github.com>
2024-12-06 10:32:58 +0100

    demo

--- app/pages/page_01.py
+++ app/pages/page_01.py
@@ -1,34 +1,34 @@
-import streamlit as st
-import requests
-import numpy as np
+# import streamlit as st
+# import requests
+# import numpy as np
 
-st.set_page_config(page_title='Hello')
+# st.set_page_config(page_title='Hello')
 
-st.title('Hello, Streamlit!')
+# st.title('Hello, Streamlit!')
 
-if st.button("Click 😉"):
-    st.image("raw_data/wagon.png")
+# if st.button("Click 😉"):
+#     st.image("raw_data/wagon.png")
 
 
-search_query = st.text_input("Search a gif")
-url = "https://api.giphy.com/v1/gifs/search"
-params = {
-    "api_key": st.secrets['API_KEY'] ,
-    "q": search_query,
-    "limit": 10
-}
+# search_query = st.text_input("Search a gif")
+# url = "https://api.giphy.com/v1/gifs/search"
+# params = {
+#     "api_key": st.secrets['API_KEY'] ,
+#     "q": search_query,
+#     "limit": 10
+# }
 
-#full_url = "https://api.giphy.com/v1/gifs/search?api_key=efd5ZBBwwanRUYg9XXMckjta9FEyUJlJ&q=dog&limit=25"
+# #full_url = "https://api.giphy.com/v1/gifs/search?api_key=efd5ZBBwwanRUYg9XXMckjta9FEyUJlJ&q=dog&limit=25"
 
-while not search_query:
-    st.stop()
+# while not search_query:
+#     st.stop()
 
-response = requests.get(url, params=params).json()
+# response = requests.get(url, params=params).json()
 
-idx = np.random.randint(0,10)
-gif_url = response["data"][idx]["embed_url"]
-st.write(
-    f'<iframe src="{gif_url}" style="height:200px;width:300px;"></iframe>',
-    unsafe_allow_html=True
-)
-st.write(response["data"][0]["title"])
+# idx = np.random.randint(0,10)
+# gif_url = response["data"][idx]["embed_url"]
+# st.write(
+#     f'<iframe src="{gif_url}" style="height:200px;width:300px;"></iframe>',
+#     unsafe_allow_html=True
+# )
+# st.write(response["data"][0]["title"])
